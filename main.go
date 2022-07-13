a12819cbf8067093179d46f162c83d247522f25a
Your Name <you@example.com>
2022-07-13 08:27:25 +0500

    1

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    //var message string
+    message := "lalala"
+    message = "trulala"
+    a := 3
+    b := 1
+    b = 2
+    var c int
+    fmt.Println(message)
+    c = a + b
+    fmt.Println(c)
+
+    const uuu string = "brbrbr"
+    
+    var m1 = ""
+    fmt.Println(m1)
+
+} 
