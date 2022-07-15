11b0b870cebcf5aa2f0018e3ec93817d18c35c1e
Your Name <you@example.com>
2022-07-15 07:21:12 +0500

    2

--- main.go
+++ main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	// "reflect"
 )
 
 func main() {
     //var message string
-    message := "lalala"
-    message = "trulala"
+    // message := "lalala"
+    // message = "trulala"
     a := 3
     b := 1
     b = 2
     var c int
-    fmt.Println(message)
+    // fmt.Println(message)
     c = a + b
     fmt.Println(c)
 
     const uuu string = "brbrbr"
-    
+    /*
+     gjgjgj
+     jgjgj
+     gjgjg
+     ggkfj    
+    */
     var m1 = ""
     fmt.Println(m1)
 
-    fmt.Println(reflect.)
+    // fmt.Println(reflect.TypeOf(message))
+    fmt.Println("*******")
+    var mmm string
+    fmt.Println(mmm)
+    fmt.Println("end file")
+    message := []byte("hello")
+    message2 := []byte("привет")
+    fmt.Println(message)
+    fmt.Println(message2)
 
+    var c1 byte
+    c1 = 35
+    fmt.Println(c1)
+
+    var sym rune = 'c'
+    fmt.Println(sym)
 } 
